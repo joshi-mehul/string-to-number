d9fb714def4d3dddf8b55d7a1ef4604da3e8c4a3
Mehul Joshi <mjoshi@yugabyte.com>
2021-03-03 13:01:03 +0530

    Added string to number code

---
+++ index.js
@@ -0,0 +1,12 @@
+const MAX_COLORS = 16777215;
+const stringToNumber = (str) => {
+  let finalNumber = 0;
+  for(let index = 0 ; index < str.length; index = index + 1) {
+    finalNumber = finalNumber + str.charCodeAt(index);
+
+    if(finalNumber >= MAX_COLORS) {
+      finalNumber = finalNumber % MAX_COLORS;
+    }
+  }
+}
+export { stringToNumber };
