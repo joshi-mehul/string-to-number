def10a88c9ed3333d88a5696bd149a3cf96192fb
Mehul Joshi <mjoshi@yugabyte.com>
2021-03-03 13:55:01 +0530

    Added bucketing logic

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-  const MAX_COLORS = 16777215;
+const MAX_COLORS = 16777215;
+const BUCKET_SIZE = 152;
+const BUCKET_NUMBER  = MAX_COLORS / BUCKET_SIZE;
 const stringToNumber = (str) => {
   let finalNumber = 0;
   for(let index = 0 ; index < str.length; index = index + 1) {
@@ -8,6 +10,7 @@ const stringToNumber = (str) => {
       finalNumber = finalNumber % MAX_COLORS;
     }
   }
-  return finalNumber;
+
+  return Math.floor((finalNumber % BUCKET_SIZE) * BUCKET_NUMBER);
 }
 export { stringToNumber };
